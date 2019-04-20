d01a605e3a65582f5d728a73c939b9bd3e31dbc0
bedewong <1005679098@qq.com>
2019-04-20 16:01:02 +0800

    [update] 剔除不需要的代码

--- service/message/msg.go
+++ service/message/msg.go
@@ -23,13 +23,3 @@ type MsgSourceStockDeal struct {
 	Stock_name	string			`json:"stock_name"`
 	Req_time	string			`json:"req_time"`
 }
-
-// 逐笔成交 数据
-type MsgTickData struct {
-	ID 		uint		`json:"id"`
-	Tick_time 	string	`json:"tick_time"`
-	Tick_code   string  `json:"tick_code"`
-	Tick_count	int		`json:"tick_count"`
-	Tick_price  float64		`json:"tick_price"`
-	Tick_Type 	string 		`json:"tick_type"`			// 交易类型： 买盘， 卖盘， 中性盘
-}
