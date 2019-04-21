d88bb7aed4b0ac7023e7edb5670aaa9e5f2762f2
bedewong <1005679098@qq.com>
2019-04-22 02:10:06 +0800

    [update] 请求结构体优化.

--- match_rpc/service/order_rpc.go
+++ match_rpc/service/order_rpc.go
@@ -136,9 +136,6 @@ type RevokeOrderRequest struct {
 	// 代替用户的token, userid+reqtime 的hash值
 	Sign		string	`json:"sign"`
 	Order_id	uint	`json:"order_id"`
-	Stock_code	string	`json:"stock_code"`
-	Stock_name	string	`json:"stock_name"`
-	Trade_type	int		`json:"trade_type"`
 	Req_time	string  `json:"req_time"`
 
 	// 比赛id： 默认非比赛情况
